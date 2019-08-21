eadd9785ddf4f743cb18977ce0c589a55fa37a45
项文龙 <xiang.xiang@pdabc.com>
2019-08-21 14:14:23 +0800

    消除真实网址 by xiang

--- app/src/main/java/com/xwl/mybasepro/config/CustomerConfig.java
+++ app/src/main/java/com/xwl/mybasepro/config/CustomerConfig.java
@@ -10,7 +10,7 @@ public class CustomerConfig {
 	public final static String HOST_STU_T = "http://192.168.10.202:18087";
 	public final static String HOST_STU_T2 = "http://192.168.10.209:18087";
 	public final static String HOST_STU_G = "http://47.111.78.238:8087";
-	public final static String HOST_STU = "https://api.aircourses.com";
+	public final static String HOST_STU = "https://api.xxxxxx.com";
 
 	// 本地保存文件
 	public static final String MEDIA_SAVE_DIR = Environment.getExternalStorageDirectory().getPath() + "/PandaabcApp/";
