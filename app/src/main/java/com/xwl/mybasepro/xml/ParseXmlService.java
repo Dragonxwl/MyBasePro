64c488afcef017c73c5aa89c3460bfb306be8223
项文龙 <xiang.xiang@pdabc.com>
2019-08-23 15:55:41 +0800

    xml 注释修改 by xiang

--- app/src/main/java/com/xwl/mybasepro/xml/ParseXmlService.java
+++ app/src/main/java/com/xwl/mybasepro/xml/ParseXmlService.java
@@ -21,7 +21,7 @@ public class ParseXmlService {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		// 通过文档构建器工厂获取一个文档构建器
 		DocumentBuilder builder = factory.newDocumentBuilder();
-		// 通过文档通过文档构建器构建一个文档实例
+		// 通过文档构建器构建一个文档实例
 		Document document = builder.parse(inStream);
 		// 获取XML文件根节点
 		Element root = document.getDocumentElement();
